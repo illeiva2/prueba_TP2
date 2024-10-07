a1e770603975ff9214f86f8040876a8b93794bc0
Ignacio Toledo Roll <rolltoledoignacio@gmail.com>
2024-10-07 19:50:29 -0300

    Refactor de la conexion para que la mantenga activa y agrego el SP para inscribir/verificar socio

--- obj/Debug/net6.0-windows/FormsPildorasInformaticas.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/FormsPildorasInformaticas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormsPildorasInformaticas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2c89a0b582ea17ad71397fedfd2cbfcfdcb210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08baba190ae91b4a40147d9ce7c293c7b1a2c17")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormsPildorasInformaticas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormsPildorasInformaticas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
